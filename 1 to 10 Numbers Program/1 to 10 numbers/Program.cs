ea3251bb1d120f55549f725a1d4abdaed43eac85
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-01-25 16:35:24 -0600

    Day2 Morning Assignments

---
+++ 1 to 10 Numbers Program/1 to 10 numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_to_10_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i <= 10; i++)
+                Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
