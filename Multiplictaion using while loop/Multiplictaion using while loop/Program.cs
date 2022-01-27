fdeea0cc112daffce609fb44e70aa5d4a9f0a56b
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-01-26 18:50:53 -0600

    Day2 Evening Assignments

---
+++ Multiplictaion using while loop/Multiplictaion using while loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplictaion_using_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, i = 1;
+            Console.WriteLine("enter any number");
+            input = Convert.ToInt32(Console.ReadLine());
+            while(i<=10)
+            { 
+        Console.WriteLine("{0}x{1}={2}",input,i,input*i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sum of 'n' Natural numbers/sum of n natural numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_n_natural_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declaration
+            int i, input, sum = 0;
+
+            //read from user
+            
+            Console.WriteLine("enter any Number");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            //logic
+
+            for (i = 0; i <= input; i++)
+                sum = sum + i;
+
+            //output
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+        }
+    }
+}
