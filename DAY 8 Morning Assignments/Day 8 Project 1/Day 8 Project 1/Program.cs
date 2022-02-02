bf0996ad0cf537c3666d568fcd9a0b4baf86aa5a
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-02 14:23:10 -0600

    Print Even Numbers using Loops

---
+++ DAY 8 Morning Assignments/Day 8 Project 1/Day 8 Project 1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_Project_1
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Print Even Numbers using Loops 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numb = new List<int>() { 22, 43, 56, 67, 88, 47, 74, 89 };
+
+            // using for Loop
+            Console.WriteLine("**************");
+            Console.WriteLine("using for loop");
+            Console.WriteLine("**************");
+
+            for (int i=0; i<Numb.Count; i++)
+            {
+                if(Numb[i]%2 == 0)
+                Console.WriteLine(Numb[i]);
+            }
+
+            // using foreach Loop
+            Console.WriteLine("******************");
+            Console.WriteLine("using foreach loop");
+            Console.WriteLine("******************");
+
+            foreach (var d in Numb)
+            {
+                if (d%2==0)
+                    Console.WriteLine(d);
+            }
+
+            // using Lambda Expression
+            Console.WriteLine("***********************");
+            Console.WriteLine("using Lambda Expression");
+            Console.WriteLine("***********************");
+
+            Numb.Where(d => d % 2 == 0).ToList().ForEach(d => Console.WriteLine(d));
+
+            // using Linq Query
+            Console.WriteLine("****************");
+            Console.WriteLine("using Linq Query");
+            Console.WriteLine("****************");
+
+            var Result = from d in Numb
+                         where d%2==0
+                         select d;
+            Result.ToList().ForEach(d => Console.WriteLine(d));
+            Console.ReadLine();
+        }
+    }
+}
