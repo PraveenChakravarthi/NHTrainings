e806839e587e67c4803e605a9f10c6fecd2a1fd8
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-04 05:44:51 -0600

    Method Overload

---
+++ DAY 10 Morning Assignments/Day 10 Project 3/Day 10 Project 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_10_Project_3
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Sample Code for Method Overloading
+
+    class MO
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public float Add(float a, float b)
+        {
+            return a + b;
+        }
+    
+        public int Add(int a, int b, int c)
+        {
+                return a + b +  c;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MO data = new MO();
+            Console.WriteLine($"Sum is : {data.Add(2.4f,4.5f)}");
+
+            Console.ReadLine();
+        }
+    }
+}
