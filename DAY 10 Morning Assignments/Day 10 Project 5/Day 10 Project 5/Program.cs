c32528369da0e3670b3563dd2c81245ece6eefb8
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-04 05:47:31 -0600

    Method OverRiding using Keyword

---
+++ DAY 10 Morning Assignments/Day 10 Project 5/Day 10 Project 5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_10_Project_5
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Sample Code for Method OverRiding using keywords
+
+    class Hyderabad
+    {
+        public void PrintHi()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        public virtual void PrintHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void PrintWU()
+        {
+            Console.WriteLine("What's up");
+        }
+    }
+
+    class Ladakh : Hyderabad
+    {
+        public override void PrintHello()
+        {
+            Console.WriteLine("Jullay");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ladakh data = new Ladakh();
+
+            Console.WriteLine("*********************");
+            Console.WriteLine("using Keywords");
+            Console.WriteLine("*********************");
+            data.PrintHi();
+            data.PrintHello();
+            data.PrintWU();
+
+
+            Console.ReadLine();
+        }
+    }
+}
