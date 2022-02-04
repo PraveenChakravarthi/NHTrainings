1b5fbde3d5e4f498eac5fc689c2b945d2770c2ce
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-04 05:46:09 -0600

    Method OverRiding

---
+++ DAY 10 Morning Assignments/Day 10 Project 4/Day 10 Project 4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_10_Project_4
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Sample Code for Method OverRiding
+
+    class Hyderabad
+    { 
+        public void PrintHi()
+        {
+            Console.WriteLine("HI");
+        }
+        
+        public void PrintHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void PrintWU()
+        {
+            Console.WriteLine("What's up");
+        }
+    }
+
+    class Ladakh : Hyderabad
+    {
+        public new void PrintHello()
+        {
+            Console.WriteLine("Jullay");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ladakh data = new Ladakh();
+            data.PrintHi();
+            data.PrintHello();
+            data.PrintWU(); 
+            
+
+            Console.ReadLine();
+        }
+    }
+}
