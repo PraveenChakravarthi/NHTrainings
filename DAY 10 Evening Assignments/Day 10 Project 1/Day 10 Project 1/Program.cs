b08cb392108c42058e4333fb653e88df716d9c1c
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-06 08:22:11 -0600

    Sample Code for Abstraction

---
+++ DAY 10 Evening Assignments/Day 10 Project 1/Day 10 Project 1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_10_Project_1
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Sample Code for Abstraction
+
+
+    abstract class Salary
+    {
+        public int GetPF(int basic)
+        {
+            return 12 * basic / 100;
+        }
+
+        public int GetHRA(int basic)
+        {
+            return 40 * basic / 100;
+        }
+
+        public abstract int GetCA();
+        public abstract int GetSA();
+    }
+    class Amazon : Salary
+    {
+        public override int GetCA()
+        {
+            return 6000;
+        }
+
+        public override int GetSA()
+        {
+            return 7000;
+        }
+    }
+
+    class Tata : Salary
+        {
+            public override int GetCA()
+            {
+                return 3000;
+            }
+
+            public override int GetSA()
+            {
+                return 6000;
+            }
+        }
+
+            class Facebook : Salary
+            {
+                public override int GetCA()
+                {
+                    return 7000;
+                }
+
+                public override int GetSA()
+                {
+                    return 9000;
+                }
+            }
+                class Microsoft : Salary
+                {
+                    public override int GetCA()
+                    {
+                        return 2000;
+                    }
+
+                    public override int GetSA()
+                    {
+                        return 6000;
+                    }
+                }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // object for Amazon
+
+            // object for Tata 
+
+            // object for Facebook
+
+            // object for Microsoft
+
+            Console.WriteLine("Completed Processing");
+            Console.ReadLine();
+        }
+    }
+}
