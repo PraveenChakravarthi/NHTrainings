7eade2da640f04bc245e520c19ab9ad01d1c5f91
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-07 05:45:03 -0600

    Employee class with only Properties

---
+++ DAY 11 Assignments/Day 11 Project 3/Day 11 Project 3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11_Project_3
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Employee class with only Properties
+
+
+    class Employee
+    {
+        public int Id
+        {
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                Id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+        public string Designation
+        {
+            set // Write only
+            {
+                Designation = value;
+            }
+        }
+        public int Salary
+        {
+            get
+            {
+                Salary = (Designation == "S") ? 30000 : 50000;
+                return Salary;
+            }
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Designation = "M";
+
+            Console.WriteLine($"Salary : {emp.Salary}");
+            Console.ReadLine();
+        }
+    }
+}
