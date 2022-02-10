0b203ee364a7f2e97197b46eddbf6a50ae7e487e
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-09 18:37:35 -0600

    2-d Array (2,2)

---
+++ DAY 13 Assignments/Day 13 Project 1/Day 13 Project 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_Project_1
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : 2-d Array (2,2)
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int [2,2];
+
+            // Array initialisation using index
+
+            data[0,0] = 1;
+            data[0,1] = 2;
+            data[1,0] = 3;
+            data[1,1] = 4;
+
+            Console.WriteLine("The given input in the form of Matrix is: ");
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(data[i, j] + " ");
+                    
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
