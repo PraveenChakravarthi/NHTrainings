2c1872213c3828cba057537c47060de2ac7efd26
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-09 18:42:15 -0600

    Reading Array values from User

---
+++ DAY 13 Assignments/Day 13 Project 4/Day 13 Project 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_Project_4
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Reading Array Values from User
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int[2, 2];
+
+            // Reading data from User
+            for (int i = 0; i < 2; i++) 
+            {
+                for (int j = 0; j < 2; j++) 
+                {
+                    Console.WriteLine("Enter Number at {0}: ",(i,j));
+                    data[i,j]=Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\n");
+
+            // Printing the data given by the user
+            Console.WriteLine("The input in Matrix form is: ");
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(data[i, j] + " ");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
