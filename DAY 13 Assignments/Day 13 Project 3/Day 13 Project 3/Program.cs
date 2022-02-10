c5470a294289947179ba178cf1868044d758b281
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-09 18:40:48 -0600

    Trace of Array

---
+++ DAY 13 Assignments/Day 13 Project 3/Day 13 Project 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_Project_3
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Trace of Array
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int[3, 3];
+            int sum=0;
+            
+            data[0, 0] = 1;
+            data[0, 1] = 3;
+            data[0, 2] = 2;
+            data[1, 0] = 9;
+            data[1, 1] = 4;
+            data[1, 2] = 5;
+            data[2, 0] = 8;
+            data[2, 1] = 7;
+            data[2, 2] = 6;
+
+            for(int i=0; i<3; i++)
+            {
+                for (int j=0; j<3; j++)
+                {
+                    // Condition for Trace
+                    if (i == j)
+                        sum = sum + data[i, j];
+                }
+            }
+            Console.WriteLine("The Trace of given Input is: ");
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
