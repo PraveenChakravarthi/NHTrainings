094239a3f91861be72d92fc577d27fbe7b7151e3
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-10 14:32:48 -0600

    Prime Number using Break

---
+++ DAY 14 Assignments/Day 14 Project 5/Day 14 Project 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_Project_5
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Prime Number program using Break
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            Console.WriteLine("Ënter any Number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for ( i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    break;
+            }
+                if (i == n)
+
+                    Console.WriteLine("The Input {0} is Prime Number", n);
+                else
+                    Console.WriteLine("The Input {0} is Composite Number", n);
+            Console.ReadLine();
+        }
+    }
+}
