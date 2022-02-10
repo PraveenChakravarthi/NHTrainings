177d2cbb6fc46b6404851f3b55b69b99e714ee30
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-09 18:39:40 -0600

    2-d Array initialising in the same line

---
+++ DAY 13 Assignments/Day 13 project 2/Day 13 project 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_project_2
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : 2-d Array (3,2)
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialising values in same the line 
+
+            int[,] data = new int[,] { { 2, 3 },{ 3, 2 },{ 4, 5 } };
+
+            Console.WriteLine("The given input in the form of Matrix: ");
+            Console.WriteLine("\n");
+
+            for (int i=0; i<3; i++)
+            {
+                for (int j=0; j<2;j++)
+                {
+                    Console.Write(data[i,j] + " ");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
