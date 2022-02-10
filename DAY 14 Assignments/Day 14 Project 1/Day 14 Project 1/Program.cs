0e3690c39c36a84b1ae051e1990300341a208426
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-10 14:28:02 -0600

    Sealed Class

---
+++ DAY 14 Assignments/Day 14 Project 1/Day 14 Project 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_Project_1
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Sealed Class
+
+    sealed class Cricket
+    {
+        public string TeamName;
+        public int TeamCount;
+    }
+
+    class Football : Cricket
+    {
+        public int Score;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cricket ct = new Cricket();
+            ct.TeamName = "SRH";
+            Football fb = new Football();
+            fb.TeamName = "CSK";
+
+            Console.WriteLine(ct.TeamName);
+
+            Console.ReadLine();
+        }
+    }
+}
