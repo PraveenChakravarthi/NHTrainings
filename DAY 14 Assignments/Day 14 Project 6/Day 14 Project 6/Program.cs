1cd61c0ad1f1bac084ba478f6207275b2ebc216a
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-10 14:34:30 -0600

    1 to 30 Numbers using continue

---
+++ DAY 14 Assignments/Day 14 Project 6/Day 14 Project 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_Project_6
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Printing Numbers between 1 & 30 and skip numbers divisible by 3 using continue
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 1; i <30; i++)
+            {
+                if (i % 3 == 0)
+                    continue;
+                Console.WriteLine(i);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
