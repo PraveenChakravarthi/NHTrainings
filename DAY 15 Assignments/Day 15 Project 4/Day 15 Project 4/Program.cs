cf340fdf7fc2eba3586d1a64f0f73ae0d53dae94
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-12 14:26:40 +0530

    Read Data from the File

---
+++ DAY 15 Assignments/Day 15 Project 4/Day 15 Project 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15_Project_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Content in the file is: ");
+            Console.WriteLine(File.ReadAllText("C:\\Day 15 Assignments\\Day 15 Project 4\\Read.txt"));
+            Console.ReadLine();
+        }
+    }
+}
