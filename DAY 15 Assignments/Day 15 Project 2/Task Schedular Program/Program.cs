0b5775069bc1632b16d229e16b463489471ad32c
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-12 14:23:35 +0530

    Task Schedular Program

---
+++ DAY 15 Assignments/Day 15 Project 2/Task Schedular Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Schedular_Program
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Task Schedular Program
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Copy("C:\\Day 15 Assignments\\Score.txt", "C:\\New folder\\Score2.txt");
+            Console.WriteLine("The File is Copied");
+            Console.ReadLine();
+        }
+    }
+}
