b776df7f426adb9c5c8105740befb8f5b77c38b3
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-14 21:43:20 +0530

    ConsoleApp using Library Class

---
+++ DAY 16 Assignments/Day 16 Project 5/Day 16 Project 5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CPraveenLibrary;
+
+namespace Day_16_Project_5
+{
+    // Author : Pravee Chakravarthi
+    // Purpose : Various Methods using Libraries
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Division is: {Mathematics.GetDiv(10, 5)}\nDistance is : {Physics.GetDistance(10, 4)}\nFormula for Gypsum : {Chemistry.GetGypsum()}");
+            
+            Console.ReadLine();
+        }
+    }
+}
