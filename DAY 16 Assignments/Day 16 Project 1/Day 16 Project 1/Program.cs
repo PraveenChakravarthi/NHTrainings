2347df019d9e52b6e912eab44add095ac592f1d3
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-14 21:13:48 +0530

    HelloWorld using OOPS

---
+++ DAY 16 Assignments/Day 16 Project 1/Day 16 Project 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_16_Project_1
+{
+    internal class Program
+    {
+        class Message
+        {
+            public static void PrintHello()
+            {
+                Console.WriteLine("Hello World");
+            }
+    
+        }
+
+        static void Main(string[] args)
+        {
+            Message.PrintHello();
+            Console.ReadLine();
+        }
+    }
+}
