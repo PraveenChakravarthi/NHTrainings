0eba5d28a25074b5675b9ad616d685d0603b2916
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-14 21:37:26 +0530

    Factorial using OOPS

---
+++ DAY 16 Assignments/Day 16 Project 2/Day 16 Project 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_16_Project_2
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Factorial using Class
+    internal class Program
+    {
+        class Factorial
+        {
+            /// <summary>
+            /// This Method Reads a Number and Returns the Factorial 
+            /// </summary>
+            public static int GetFactorial()
+            {
+                int n;
+                Console.WriteLine("Enter any Numbber: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                int fact = 1;
+                for (int i = 1; i <=n; i++ )
+                    fact = fact * i;
+                return fact;
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Factorial is: { Factorial.GetFactorial()}");
+            Console.ReadLine();
+            
+        }
+    }
+}
