3d5969d770ad71134da7f1c58b52ec02d312fc0f
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-14 21:45:10 +0530

    Multiplication Table of a Number using OOPS

---
+++ DAY 16 Assignments/Day 16 Project 6/Day 16 Project 6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_16_Project_6
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Multiplication using OOPS Concept
+    internal class Program
+    {
+        class MultiplicationTable
+        {
+            /// <summary>
+            /// This Method gives the Multiplication of a given Number
+            /// </summary>
+            public static void GetMultiplication(int n)
+            {
+                
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} x {i} = {n*i}");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication table for 10: \n");
+            MultiplicationTable.GetMultiplication(10);
+            Console.ReadLine();
+        }
+    }
+}
