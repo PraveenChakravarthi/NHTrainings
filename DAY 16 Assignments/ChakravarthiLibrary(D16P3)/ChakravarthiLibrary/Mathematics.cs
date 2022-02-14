0f75a79cc138192b396728ef3384027c83eb6190
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-14 21:39:09 +0530

    Class Library Project

---
+++ DAY 16 Assignments/ChakravarthiLibrary(D16P3)/ChakravarthiLibrary/Mathematics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChakravarthiLibrary
+{
+    public class Mathematics
+    {
+        int n;
+        public void ReadInput()
+        { 
+            Console.WriteLine("Enter any Number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+            public int GetFactorial()
+            {
+                int fact = 1;
+                for (int i = 1; i <= n; i++)
+                    fact = fact * i;
+                 return fact; 
+        }
+    }
+}
+
