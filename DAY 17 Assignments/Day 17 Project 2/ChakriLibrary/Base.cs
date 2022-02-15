7f818ddc5a2e165bdd6ed9a9cfa76d7e0cd9c905
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-15 12:24:54 +0530

    Testing Access Modifiers Programusing Libraries

---
+++ DAY 17 Assignments/Day 17 Project 2/ChakriLibrary/Base.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChakriLibrary
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : ChakriLibrary for Testing of Access Modifiers
+    public class Base  // ChakriBase Class
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+        /// <summary>
+        /// This Method Reads Data from Base Class
+        /// </summary>
+        public void ReadBaseData()
+        {
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+        }
+    }
+
+    public class Derived : Base  //ChakriDerived Class inherited by Base Class
+    {
+        /// <summary>
+        /// This Methods Read Data from Derived Class
+        /// </summary>
+        public void ReadDerivedData()
+        {
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+        }
+    }
+
+    public class Other   //ChakriOther Class
+    {
+        /// <summary>
+        /// This Method Reads Data from Other Class 
+        /// </summary>
+       public void ReadOtherData()
+        {
+           Base B = new Base();
+            B.a = 1;
+            B.b = 2;
+            B.c = 3;
+            B.d = 4;
+            B.e = 5;
+        }
+        
+    }
+}
+
---
+++ DAY 17 Assignments/Day 17 Project 2/PublicLibrary/Base.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChakriLibrary;
+
+namespace PublicLibrary
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : PublicLibrary for testing Access Modifiers
+    public class PLBase : Base   // PublicBase Class inherited by ChakriBase Class
+    {
+        /// <summary>
+        /// This Method Reads Data from Public Base Class
+        /// </summary>
+        public void ReadPBaseData()
+        {
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+        }
+    }
+        public class PLOther // PublicOther Class
+    {
+        /// <summary>
+        /// This Method Reads Data from Public Other Class
+        /// </summary>
+        public void ReadPOtherData()
+        {
+            Base B = new Base();
+            B.a = 1;
+            B.b = 2;
+            B.c = 3;
+            B.d = 4;
+            B.e = 5;
+        }
+    }
+}
