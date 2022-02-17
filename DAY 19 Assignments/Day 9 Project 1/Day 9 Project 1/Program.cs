43808e1c32701dd4317c93e02f04dbb8a0f631f5
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-17 20:50:39 +0530

    C# Code to Read XML Files

---
+++ DAY 19 Assignments/Day 9 Project 1/Day 9 Project 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Day_9_Project_1
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Reading XML Files using C#
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument XD = new XmlDocument();
+            XD.Load("C:\\Day 19 Assignments\\MyData(TagBased).xml");
+
+            foreach (XmlNode node in XD.DocumentElement.ChildNodes)
+            {
+                string text = node.InnerText;
+
+                Console.WriteLine(text);
+            }
+            Console.ReadLine();
+        }
+    }
+}
