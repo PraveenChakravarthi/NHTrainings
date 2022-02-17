a12343084d276d53d615ef3b6905b0e5d269ee43
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-17 20:52:32 +0530

    C# Code to Read XML File and Print only Customer Name

---
+++ DAY 19 Assignments/Day 9 Project 2/Day 9 Project 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace Day_9_Project_2
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Reading XML using C# and Printing only Names
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument XD = new XmlDocument();
+            XD.Load("C:\\Day 19 Assignments\\MyData(TagBased).xml");
+
+            foreach (XmlNode node in XD.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode childNode in node.ChildNodes)
+                    if (childNode.Name == "Name")
+                        Console.WriteLine(childNode.InnerText);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
