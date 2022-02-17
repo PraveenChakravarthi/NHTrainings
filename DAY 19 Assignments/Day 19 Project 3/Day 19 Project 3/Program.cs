c8771af18ccd53439208c7f80bdf0a55bc91beb4
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-17 20:54:21 +0530

    C# Code to Print XML File Content with ,

---
+++ DAY 19 Assignments/Day 19 Project 3/Day 19 Project 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Day_19_Project_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument XD = new XmlDocument();
+            XD.Load("C:\\Day 19 Assignments\\MyData(TagBased).xml");
+
+            foreach (XmlNode node in XD.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode childNode in node.ChildNodes)
+                {
+                    if (childNode.Name == "ID")
+                        Console.Write(childNode.InnerText + ",");
+                    if (childNode.Name == "Name")
+                        Console.Write(childNode.InnerText+ ",");
+                    if (childNode.Name == "Age")
+                        Console.Write(childNode.InnerText+ "\n");
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
