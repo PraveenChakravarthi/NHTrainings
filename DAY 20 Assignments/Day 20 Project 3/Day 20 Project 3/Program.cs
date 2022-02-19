d90105228b41d9b830dcae5b7830cae5aed93529
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-19 13:18:23 +0530

    Usage of out and ref Parameters

---
+++ DAY 20 Assignments/Day 20 Project 3/Day 20 Project 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20_Project_3
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Usage of Out, Ref Parameters in C#
+    public class Program
+    { 
+        public static void Out(out int a)
+        {
+            a = 5;
+        }
+        public static void Ref(ref int b)
+        {
+            b = 10;
+        }
+        static void Main(string[] args)
+        {
+            int c;
+            int d = 3;
+
+            Out(out c); // Updating the 'c' value using out parameter
+            Ref(ref d); // Changing the 'd' value using ref parameter
+
+            Console.WriteLine($"Updated Value is: {c}");
+            Console.WriteLine($"Changed Value is: {d}");
+            Console.ReadLine();
+        }
+    }
+}
