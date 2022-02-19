a1878da611a38e36cd554f9256788aac8951bfc1
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-19 13:16:37 +0530

    Nullable Types in C#

---
+++ DAY 20 Assignments/Day 20 Project 2/Day 20 Project 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20_Project_2
+{
+    // Author : Praveen Chakravarthi
+    // Purpose : Nullable Types in C#
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? ID = 3; // Declaring Nullable to Value Type using '?'
+            
+            if (ID.HasValue) // Checks if ID has a Value
+                Console.WriteLine($"ID is {ID}");
+            else
+                Console.WriteLine("No Value");
+            Console.ReadLine();
+        }
+    }
+}
