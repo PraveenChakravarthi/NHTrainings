6617baa291a19d5313e21bc0775d2956b26601fd
PraveenChakravarthi <praveenchakravarthi0514@gmail.com>
2022-02-21 12:50:46 +0530

    Console and Desktop Apps consuming WebServices

---
+++ DAY 21 Assignments/MyClientApp/MyClientApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyClientApp.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyClientApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperationsSoapClient MO = new MathOperationsSoapClient();
+            Console.WriteLine($"Factorial is : {MO.Factorial(5)}");
+            Console.WriteLine($"Sum is : {MO.Add(3,5)}");
+            Console.WriteLine($"Product is : {MO.Mul(2,3)}");
+            Console.WriteLine($"Difference is : {MO.Sub(5,2)}");
+            Console.ReadLine();
+        }
+    }
+}
